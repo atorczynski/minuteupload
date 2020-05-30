6efe4360e2c7554d4a0b82fadc28fb9c7e05e275
atorczynski <a.torczynski@gmail.com>
2020-05-30 03:21:39 +0200

    Render component to app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom';
+
 import GlobalStyles from './GlobalStyles';
 import { TopBar } from './components/NavBar/NavBar';
+import UploadContainer from './components/UploadWindow/UploadContainer';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <TopBar />
+      <Router>
+        <TopBar />
+        <Switch>
+          <Route to={'/'}>
+            <UploadContainer />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
