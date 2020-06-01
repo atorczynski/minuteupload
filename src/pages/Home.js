b17e197449699fed8fd988aaacb85c7e48c8bd82
atorczynski <a.torczynski@gmail.com>
2020-06-01 22:30:05 +0200

    Add first page to app.

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UploadContainer from '../components/UploadWindow/UploadContainer';
+
+export default function Home() {
+  return (
+    <div>
+      <UploadContainer />
+    </div>
+  );
+}
