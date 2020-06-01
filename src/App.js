4ddc23b5a005198774bfa89877c5aa677b52b1ab
atorczynski <a.torczynski@gmail.com>
2020-06-01 22:28:42 +0200

    Cleanup app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink,
-  Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import GlobalStyles from './GlobalStyles';
 import { TopBar } from './components/NavBar/NavBar';
-import UploadContainer from './components/UploadWindow/UploadContainer';
 import Home from './pages/Home';
 
 function App() {
